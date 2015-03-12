5510ddb2b5f5e58941f111818b9c3e276d7b4412
Tamas Szabo <tamas.szabo@doclerholding.com>
2015-03-12 16:13:32 +0100

    1. use the same class in the second function, I have to move it to the setup

--- test/src/MineDetectorTest.php
+++ test/src/MineDetectorTest.php
@@ -18,5 +18,15 @@ class StringToArrayTest extends PHPUnit_Framework_TestCase
 		$this->assertTrue(is_array($stringToArray->convert()));
 	}
 
+	/**
+	 * Test validate input with wrong params
+	 *
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testValidateInput()
+	{
+		$stringToArray = new StringToArray();
+	}
+
 
 }
