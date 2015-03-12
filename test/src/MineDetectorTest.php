ca3a54c77f17dac886b8132b3ea3775908c84bcb
Tamas Szabo <tamas.szabo@doclerholding.com>
2015-03-12 16:42:36 +0100

    1. was some mistake in the provide structure, fixed it, and try to solve the second function

--- test/src/MineDetectorTest.php
+++ test/src/MineDetectorTest.php
@@ -33,18 +33,43 @@ class StringToArrayTest extends PHPUnit_Framework_TestCase
 	}
 
 	/**
+	 * Test the split by commas.
+	 *
+	 * @dataProvider splitByCommasProvider
+	 */
+	public function testSplitByCommas($expectedResult, $convertableParam)
+	{
+		$convertedResult = $this->stringToArrayEntity->convert($convertableParam);
+
+		$this->assertEquals($expectedResult, $convertableParam);
+	}
+
+	/**
 	 * Provider for input validation with wrong inputs.
 	 */
 	public function validateInputProvider()
 	{
 		return array(
-			array(
-				'null'    => array(''),
-				'integer' => 34,
-				'array'   => array(1,23,4,5),
-				'float'   => 3.234,
-				'boolean' => true
-			),
+			'null'    => array(array('')),
+			'integer' => array(34),
+			'array'   => array(array(1,23,4,5)),
+			'float'   => array(3.234),
+			'boolean' => array(true)
+		);
+	}
+
+	/**
+	 * Provider for input splitByCommas.
+	 */
+	public function splitByCommasProvider()
+	{
+		return array(
+			'one data with empty' => array(array('almafa'), 'almafa'),
+//			'alone'               => array(array(), ','),
+//			'two values'          => array(array('almafa', 'balmafa'), 'almafa,balmafa'),
+//			'example1'            => array(array('a','b','c'), 'a,b,c'),
+//			'example2'            => array(array('100','982','444','990','1'), '100,982,444,990,1'),
+//			'example3'            => array(array('Mark','Anthony','marka@lib.de'), 'Mark,Anthony,marka@lib.de'),
 		);
 	}
 
