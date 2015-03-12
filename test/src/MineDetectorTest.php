fd1ef95fe5e7f30453869d5b13a41c89c916d807
Tamas Szabo <tamas.szabo@doclerholding.com>
2015-03-12 16:25:34 +0100

    1. now the input validation is ok

--- test/src/MineDetectorTest.php
+++ test/src/MineDetectorTest.php
@@ -21,16 +21,6 @@ class StringToArrayTest extends PHPUnit_Framework_TestCase
 	}
 
 	/**
-	 * Test gets a valid array for return.
-	 *
-	 * @returns void
-	 */
-	public function testExistClass()
-	{
-		$this->assertTrue(is_array($this->stringToArrayEntity->convert()));
-	}
-
-	/**
 	 * Test validate input with wrong params
 	 *
 	 * @expectedException InvalidArgumentException
