271bc395365a6e88a7f2fd9ec5bbf995b1fa71a3
Tamas Szabo <tamas.szabo@doclerholding.com>
2015-03-12 17:15:18 +0100

    3. Create Do for passing data.

---
+++ src/ConvertedDo.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Data object for the 3. exercise.
+ *
+ */
+class ConvertedDo {
+	/**
+	 * An array for passing the converted data.
+	 *
+	 * @var array
+	 */
+	private $convertedData;
+
+	/**
+	 * Gets the array of the converted data.
+	 *
+	 * @return array
+	 */
+	public function getConvertedData()
+	{
+		return $this->convertedData;
+	}
+
+	/**
+	 * Set the converted values.
+	 *
+	 * @param array $convertedData  The converted data.
+	 */
+	public function setConvertedData(array $convertedData)
+	{
+		$this->convertedData = $convertedData;
+	}
+}
